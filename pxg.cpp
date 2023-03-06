7aaa567cfc6f5e4e0b0bb7802b27d3a467d586de
Zhou Chang <achang.zhou@gmail.com>
2023-03-06 13:50:16 +0800

    clean code

--- pxg.cpp
+++ pxg.cpp
@@ -3,13 +3,15 @@
 #include <mpi.h>
 #include <nccl.h>
 
+int main(int argc, char* argv[]) {
+    MPI_Init(&argc, &argv);
 
+    int world;
+    int rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &world);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-int main() {
-    MPI_Init(NULL, NULL);
-
-
-
+    std::cout << world << " / " << rank << std::endl;
 
     MPI_Finalize();
 }
